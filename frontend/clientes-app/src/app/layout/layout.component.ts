1630f00ecb362b24c72700e48f0faf25a243349f
IgorLima <igorlimadrum@gmail.com>
2023-07-03 23:01:57 -0300

    mudando arquivos para este repositório

---
+++ frontend/clientes-app/src/app/layout/layout.component.ts
@@ -0,0 +1,32 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    (function ($) {
+      "use strict";
+
+      // Add active state to sidebar nav links
+      var path = window.location.href;
+      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+        if ((this as HTMLAnchorElement).href === path) {
+          $(this).addClass("active");
+        }
+      });
+
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function (e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })($);
+  }
+}
